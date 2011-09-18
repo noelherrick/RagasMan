c9c9a412f201aa17709373663cf171cb35dd2912
Noel Herrick <noelherrick@gmail.com>
2011-09-18 16:22:04 -0600

    Initial Commit

---
+++ Anagram/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Anagram
+//
+//  Created by Noel Herrick on 9/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
